233334018ec28411af19dab3412b70b8f977c3d1
wagle04 <waglemanish28@gmail.com>
2019-02-27 18:39:33 +0545

    1st commit

---
+++ src/classes/operators_in_java.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author user
+ */
+public class operators_in_java {
+    public static void main(String args[]){
+        System.out.println("Integer Arithmetic");
+        
+    }
+}
