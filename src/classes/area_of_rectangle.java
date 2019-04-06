0f81e7df53b089a92c4cf6e5b8f116d295d87de7
wagle04 <waglemanish28@gmail.com>
2019-04-06 18:16:22 +0545

    new IDE

--- src/classes/area_of_rectangle.java
+++ src/classes/area_of_rectangle.java
@@ -8,23 +8,26 @@ package classes;
 
 import java.util.Scanner;
 
-class rectangle{
+class rectangle {
     float length;
     float breadth;
-    
-    float area(float l,float b){
-        return l*b;
+
+    float area(float l, float b) {
+        length = l;
+        breadth = b;
+        return length * breadth;
     }
 }
+
 public class area_of_rectangle {
-    public static void main(String args[]){
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter length:");
-    float len = input.nextFloat();
-    System.out.println("Enter breadth:");
-    float bre = input.nextFloat();
-    
-    rectangle rect=new rectangle();
-    System.out.println("Area of rectangle= "+rect.area(len,bre));
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter length:");
+        float len = input.nextFloat();
+        System.out.println("Enter breadth:");
+        float bre = input.nextFloat();
+
+        rectangle rect = new rectangle();
+        System.out.println("Area of rectangle= " + rect.area(len, bre));
     }
 }
