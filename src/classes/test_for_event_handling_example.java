7cd149e65d6651f2fc841b8be08ab3ef11a78b9a
wagle04 <waglemanish28@gmail.com>
2019-05-06 07:25:19 +0545

    lab assignment 15

---
+++ src/classes/test_for_event_handling_example.java
@@ -0,0 +1,14 @@
+
+package classes;
+import javax.swing.JFrame;
+
+public class test_for_event_handling_example {
+    public static void main(String[] args){
+        event_handling_example textFieldFrame = new event_handling_example();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350,100);
+        textFieldFrame.setVisible(true);
+        
+    }
+    
+}
